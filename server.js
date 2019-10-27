c5c8cde5845aaf41bc8e1f9b28fd41847204973e
Gold Dust <48570104+ashley-blue@users.noreply.github.com>
2019-10-26 22:19:38 -0600

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 6969;
+const port = process.env.PORT || 3000;
 
 const handlebars = require('express-handlebars');
 
